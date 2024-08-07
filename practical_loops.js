1ca070f9483aa8564b8e40ec75e14348c45cb39f
BlueUnderBoy <lital5@yahoo.com>
2024-08-07 14:39:53 -0500

    PT update

--- practical_loops.js
+++ practical_loops.js
@@ -15,7 +15,11 @@ for (i = 0; i<101; i++) {
 }
 */
 //Prime Time
-if (n>1) {
+let n = 3;
+if (n == 2 || n == 3 || n == 5 || n == 7) {
+    console.log(n)
+}
+else if (n>1) {
     while (n%2 == 0 || n%3 ==0 || n%5 == 0 || n%7 == 0) {
         n++
     }
