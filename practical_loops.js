649daaef1d3311d74bc4fec30e402c2aee528ae1
Jalen Young <lital5@yahoo.com>
2024-08-07 18:21:46 -0500

    FL update

--- practical_loops.js
+++ practical_loops.js
@@ -35,3 +35,6 @@ else {
 }
 */
 //Feeling Loopy
+string = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+n = string.length
+console.log(n)
